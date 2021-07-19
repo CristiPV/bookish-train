3b833b27c5160948e0d53cd4c87a96a5c9de4a8b
Cristian <cristi.valentin29@gmail.com>
2021-07-19 12:56:14 +0300

    Add answer widget

---
+++ flutter_guide/lib/answer.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final String answerText;
+  final VoidCallback answerCallback;
+
+  Answer(this.answerText, this.answerCallback);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(Colors.orange[400]),
+          textStyle: MaterialStateProperty.all(
+            TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+        onPressed: answerCallback,
+        child: Text(answerText),
+      ),
+    );
+  }
+}
